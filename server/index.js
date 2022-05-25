9bc1ad38ab0dec567551b52e3e34b43c17f76e51
Suthan PC <suthan0015@gmail.com>
2022-05-25 12:51:39 +0530

    completed

--- server/index.js
+++ server/index.js
@@ -3,20 +3,22 @@ var app = express();
 const cors = require("cors");
 var fs = require("fs");
 var corsOptions = {
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000", "*"],
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/read", function (req, res) {
+app.get("/read", async function (req, res) {
   console.log("Got a GET request for the homepage s");
-  console.log(req);
-  res.send("Hello GET");
+  fs.readFile("../public/user/data.json", (err, data) => {
+    res.send(JSON.parse(data));
+    return;
+  });
 });
 
-app.post("/write", function (req, res) {
+app.post("/write", async function (req, res) {
   console.log(req.body);
   let products = [];
   fs.readFile("../public/user/data.json", (err, data) => {
--- src/App.js
+++ src/App.js
@@ -1,64 +1,100 @@
-import { Button, Col, Form, Input, Row } from "antd";
+import { Button, Col, Divider, Form, Input, InputNumber, Row } from "antd";
 import axios from "axios";
-
-function onFinish(val) {
-  //Send input field value to server via axios
-  console.log("Send post request", val);
-  axios
-    .post("http://127.0.0.1:5000/write", {
-      name: val.name,
-      age: val.age,
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
-
-function handleRead() {
-  //Receive file from server and read it
-}
+import { useState } from "react";
 
 function App() {
+  const [userData, setUserData] = useState([]);
+
+  function onFinish(val) {
+    //Send input field value to server via axios
+    console.log("Send post request", val);
+    axios
+      .post("http://127.0.0.1:5000/write", {
+        name: val.name,
+        age: val.age,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  async function getData() {
+    try {
+      const response = await axios.get("http://127.0.0.1:5000/read");
+      console.log(response.data);
+      setUserData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <Form onFinish={onFinish}>
-      <Form.Item
-        label="Name"
-        name="name"
-        rules={[
-          {
-            required: true,
-            message: "Please input your name!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Age"
-        name="age"
-        rules={[
-          {
-            required: true,
-            message: "Please input your age!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Row gutter={10}>
-        <Col>
-          <Button htmlType="submit" type="primary">
-            Submit
-          </Button>
-        </Col>
-        <Col>
-          <Button type="dashed">Read Data</Button>
-        </Col>
-      </Row>
-    </Form>
+    <Row gutter={10}>
+      <Col md={12}>
+        <Form
+          onFinish={onFinish}
+          labelCol={{
+            span: 0,
+          }}
+          wrapperCol={{
+            span: 5,
+          }}
+        >
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: "Please input your name!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Age"
+            name="age"
+            rules={[
+              {
+                required: true,
+                message: "Please input your age!",
+              },
+            ]}
+          >
+            <InputNumber min={1} max={99} defaultValue={0} />
+          </Form.Item>
+          <Row gutter={10}>
+            <Col>
+              <Button htmlType="submit" type="primary">
+                Submit
+              </Button>
+            </Col>
+            <Col>
+              <Button type="dashed" onClick={getData}>
+                Read Data
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Col>
+      <Col>
+        {userData.map((val) => {
+          return (
+            <>
+              <Col key={val.name}>
+                <Row>Name: {val.name}</Row>
+                <Row>Age: {val.age}</Row>
+              </Col>
+              <Divider />
+            </>
+          );
+        })}
+      </Col>
+    </Row>
   );
 }
 
